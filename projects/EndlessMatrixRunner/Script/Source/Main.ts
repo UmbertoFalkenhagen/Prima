7d34ecfead78567430b4ecd3172c0769aa316c84
Umberto <umberto.falkenhagen@hs-furtwangen.de>
2022-01-03 11:51:08 +0100

    set up final project

---
+++ projects/EndlessMatrixRunner/Script/Source/Main.ts
@@ -0,0 +1,61 @@
+namespace EndlessMatrixRunner {
+  import ƒ = FudgeCore;
+
+  let viewport: ƒ.Viewport;
+  let sceneGraph: ƒ.Node;
+
+  let cameraNode: ƒ.Node;
+
+  ƒ.Debug.info("Main Program Template running!");
+
+  window.addEventListener("load", init);
+
+  function init(_event: Event): void {
+    let dialog: HTMLDialogElement = document.querySelector("dialog");
+    dialog.querySelector("h1").textContent = document.title;
+    dialog.addEventListener("click", (_event: Event) => {
+        // @ts-ignore until HTMLDialog is implemented by all browsers and available in dom.d.ts
+        dialog.close();
+        start();
+      });
+    //@ts-ignore
+    dialog.showModal();
+}
+
+  async function start(): Promise<void> {
+    await ƒ.Project.loadResourcesFromHTML();
+    sceneGraph = <ƒ.Graph>ƒ.Project.resources["Graph|2021-11-18T14:33:59.117Z|18376"];
+
+    let cmpCamera: ƒ.ComponentCamera  = new ƒ.ComponentCamera();
+
+    let canvas: HTMLCanvasElement = document.querySelector("canvas");
+    viewport = new ƒ.Viewport();
+    viewport.initialize("Viewport", sceneGraph, cmpCamera, canvas);
+    sceneGraph = viewport.getBranch();
+
+    viewport.calculateTransforms();
+
+    ƒ.AudioManager.default.listenTo(sceneGraph);
+    ƒ.AudioManager.default.listenWith(sceneGraph.getComponent(ƒ.ComponentAudioListener));
+
+    ƒ.AudioManager.default.listenTo(sceneGraph);
+    ƒ.AudioManager.default.listenWith(sceneGraph.getComponent(ƒ.ComponentAudioListener));
+
+    cmpCamera.mtxPivot.translation = new ƒ.Vector3(0, 8, -12);
+    cmpCamera.mtxPivot.rotation = new ƒ.Vector3(25, 0, 0);
+    
+    cameraNode = new ƒ.Node("cameraNode");
+    cameraNode.addComponent(cmpCamera);
+    cameraNode.addComponent(new ƒ.ComponentTransform);
+    sceneGraph.addChild(cameraNode);
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+  }
+
+  function update(_event: Event): void {
+    // ƒ.Physics.world.simulate();  // if physics is included and used
+    viewport.draw();
+    ƒ.AudioManager.default.update();
+  }
+}
