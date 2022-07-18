10e8a97ed24075df99eafdfcb387677af95d0e45
Umberto <umberto.falkenhagen@hs-furtwangen.de>
2022-07-18 14:01:53 +0200

    corrected fetch

--- projects/EndlessMatrixRunnerSoSe22/Script/Source/Main.ts
+++ projects/EndlessMatrixRunnerSoSe22/Script/Source/Main.ts
@@ -156,7 +156,7 @@ namespace EndlessMatrixRunnerSoSe22 {
     async function fetchData() {
       try {
         // tslint:disable-next-line: typedef
-        const response = await fetch("../configuration.JSON");
+        const response = await fetch("https://github.com/UmbertoFalkenhagen/Prima/blob/master/projects/EndlessMatrixRunnerSoSe22/configuration.json");
         // tslint:disable-next-line: typedef
         const responseObj = await response.json();
         return responseObj;
